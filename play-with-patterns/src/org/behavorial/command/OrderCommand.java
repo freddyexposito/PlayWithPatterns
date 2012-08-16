c9615911bb6eab0df87a664c03028c085eb67dc6
Freddy Exposito <exposito@gmail.com>
2012-08-16 16:29:28 -0400

    deleting wrong package name

--- play-with-patterns/src/org/behavorial/command/OrderCommand.java
+++
@@ -1,5 +0,0 @@
-package org.behavorial.command;
-
-public interface OrderCommand {
-	void execute();
-}
--- play-with-patterns/src/org/behavorial/strategy/Strategy.java
+++
@@ -1,5 +0,0 @@
-package org.behavorial.strategy;
-
-public interface Strategy {
-	int execute (int x, int y);
-}
